f0cf869861d2a9cef248ecc6d9dd9c7fde50298a
Atsushi Nishiyama <cp527242@kc4.so-net.ne.jp>
2018-12-09 17:14:15 +0900

    とりあえず、動いた。 が、まだ異常終了するパターンあり

--- AzCalc/Form1.Designer.vb
+++ AzCalc/Form1.Designer.vb
@@ -22,10 +22,274 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox_Work = New System.Windows.Forms.TextBox()
+        Me.Button_One = New System.Windows.Forms.Button()
+        Me.Button_Two = New System.Windows.Forms.Button()
+        Me.Button_Three = New System.Windows.Forms.Button()
+        Me.Button_Four = New System.Windows.Forms.Button()
+        Me.Button_Five = New System.Windows.Forms.Button()
+        Me.Button_Six = New System.Windows.Forms.Button()
+        Me.Button_Seven = New System.Windows.Forms.Button()
+        Me.Button_Eight = New System.Windows.Forms.Button()
+        Me.Button_Nine = New System.Windows.Forms.Button()
+        Me.Button_Plus = New System.Windows.Forms.Button()
+        Me.Button_Minus = New System.Windows.Forms.Button()
+        Me.Button_Asterisk = New System.Windows.Forms.Button()
+        Me.Button_Slash = New System.Windows.Forms.Button()
+        Me.Button_Equal = New System.Windows.Forms.Button()
+        Me.Button_LeftParen = New System.Windows.Forms.Button()
+        Me.Button_RightParen = New System.Windows.Forms.Button()
+        Me.Button_Zero = New System.Windows.Forms.Button()
+        Me.TextBox_Result = New System.Windows.Forms.TextBox()
+        Me.Button_Clear = New System.Windows.Forms.Button()
+        Me.Button_Period = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox_Work
+        '
+        Me.TextBox_Work.Font = New System.Drawing.Font("MS UI Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox_Work.Location = New System.Drawing.Point(9, 12)
+        Me.TextBox_Work.Name = "TextBox_Work"
+        Me.TextBox_Work.ReadOnly = True
+        Me.TextBox_Work.Size = New System.Drawing.Size(284, 31)
+        Me.TextBox_Work.TabIndex = 0
+        Me.TextBox_Work.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button_One
+        '
+        Me.Button_One.Location = New System.Drawing.Point(9, 73)
+        Me.Button_One.Name = "Button_One"
+        Me.Button_One.Size = New System.Drawing.Size(52, 49)
+        Me.Button_One.TabIndex = 1
+        Me.Button_One.Text = "1"
+        Me.Button_One.UseVisualStyleBackColor = True
+        '
+        'Button_Two
+        '
+        Me.Button_Two.Location = New System.Drawing.Point(67, 73)
+        Me.Button_Two.Name = "Button_Two"
+        Me.Button_Two.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Two.TabIndex = 2
+        Me.Button_Two.Text = "2"
+        Me.Button_Two.UseVisualStyleBackColor = True
+        '
+        'Button_Three
+        '
+        Me.Button_Three.Location = New System.Drawing.Point(125, 73)
+        Me.Button_Three.Name = "Button_Three"
+        Me.Button_Three.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Three.TabIndex = 3
+        Me.Button_Three.Text = "3"
+        Me.Button_Three.UseVisualStyleBackColor = True
+        '
+        'Button_Four
+        '
+        Me.Button_Four.Location = New System.Drawing.Point(9, 128)
+        Me.Button_Four.Name = "Button_Four"
+        Me.Button_Four.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Four.TabIndex = 4
+        Me.Button_Four.Text = "4"
+        Me.Button_Four.UseVisualStyleBackColor = True
+        '
+        'Button_Five
+        '
+        Me.Button_Five.Location = New System.Drawing.Point(67, 128)
+        Me.Button_Five.Name = "Button_Five"
+        Me.Button_Five.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Five.TabIndex = 5
+        Me.Button_Five.Text = "5"
+        Me.Button_Five.UseVisualStyleBackColor = True
+        '
+        'Button_Six
+        '
+        Me.Button_Six.Location = New System.Drawing.Point(125, 128)
+        Me.Button_Six.Name = "Button_Six"
+        Me.Button_Six.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Six.TabIndex = 6
+        Me.Button_Six.Text = "6"
+        Me.Button_Six.UseVisualStyleBackColor = True
+        '
+        'Button_Seven
+        '
+        Me.Button_Seven.Location = New System.Drawing.Point(9, 183)
+        Me.Button_Seven.Name = "Button_Seven"
+        Me.Button_Seven.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Seven.TabIndex = 7
+        Me.Button_Seven.Text = "7"
+        Me.Button_Seven.UseVisualStyleBackColor = True
+        '
+        'Button_Eight
+        '
+        Me.Button_Eight.Location = New System.Drawing.Point(67, 183)
+        Me.Button_Eight.Name = "Button_Eight"
+        Me.Button_Eight.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Eight.TabIndex = 8
+        Me.Button_Eight.Text = "8"
+        Me.Button_Eight.UseVisualStyleBackColor = True
+        '
+        'Button_Nine
+        '
+        Me.Button_Nine.Location = New System.Drawing.Point(125, 183)
+        Me.Button_Nine.Name = "Button_Nine"
+        Me.Button_Nine.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Nine.TabIndex = 9
+        Me.Button_Nine.Text = "9"
+        Me.Button_Nine.UseVisualStyleBackColor = True
+        '
+        'Button_Plus
+        '
+        Me.Button_Plus.Location = New System.Drawing.Point(241, 73)
+        Me.Button_Plus.Name = "Button_Plus"
+        Me.Button_Plus.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Plus.TabIndex = 12
+        Me.Button_Plus.Text = "+"
+        Me.Button_Plus.UseVisualStyleBackColor = True
+        '
+        'Button_Minus
+        '
+        Me.Button_Minus.Location = New System.Drawing.Point(241, 129)
+        Me.Button_Minus.Name = "Button_Minus"
+        Me.Button_Minus.Size = New System.Drawing.Size(52, 48)
+        Me.Button_Minus.TabIndex = 13
+        Me.Button_Minus.Text = "-"
+        Me.Button_Minus.UseVisualStyleBackColor = True
+        '
+        'Button_Asterisk
+        '
+        Me.Button_Asterisk.Location = New System.Drawing.Point(241, 185)
+        Me.Button_Asterisk.Name = "Button_Asterisk"
+        Me.Button_Asterisk.Size = New System.Drawing.Size(52, 47)
+        Me.Button_Asterisk.TabIndex = 14
+        Me.Button_Asterisk.Text = "*"
+        Me.Button_Asterisk.UseVisualStyleBackColor = True
+        '
+        'Button_Slash
+        '
+        Me.Button_Slash.Location = New System.Drawing.Point(241, 238)
+        Me.Button_Slash.Name = "Button_Slash"
+        Me.Button_Slash.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Slash.TabIndex = 15
+        Me.Button_Slash.Text = "/"
+        Me.Button_Slash.UseVisualStyleBackColor = True
+        '
+        'Button_Equal
+        '
+        Me.Button_Equal.Location = New System.Drawing.Point(125, 238)
+        Me.Button_Equal.Name = "Button_Equal"
+        Me.Button_Equal.Size = New System.Drawing.Size(110, 49)
+        Me.Button_Equal.TabIndex = 16
+        Me.Button_Equal.Text = "="
+        Me.Button_Equal.UseVisualStyleBackColor = True
+        '
+        'Button_LeftParen
+        '
+        Me.Button_LeftParen.Location = New System.Drawing.Point(183, 73)
+        Me.Button_LeftParen.Name = "Button_LeftParen"
+        Me.Button_LeftParen.Size = New System.Drawing.Size(52, 49)
+        Me.Button_LeftParen.TabIndex = 17
+        Me.Button_LeftParen.Text = "("
+        Me.Button_LeftParen.UseVisualStyleBackColor = True
+        '
+        'Button_RightParen
+        '
+        Me.Button_RightParen.Location = New System.Drawing.Point(183, 129)
+        Me.Button_RightParen.Name = "Button_RightParen"
+        Me.Button_RightParen.Size = New System.Drawing.Size(52, 47)
+        Me.Button_RightParen.TabIndex = 18
+        Me.Button_RightParen.Text = ")"
+        Me.Button_RightParen.UseVisualStyleBackColor = True
+        '
+        'Button_Zero
+        '
+        Me.Button_Zero.Location = New System.Drawing.Point(9, 238)
+        Me.Button_Zero.Name = "Button_Zero"
+        Me.Button_Zero.Size = New System.Drawing.Size(52, 49)
+        Me.Button_Zero.TabIndex = 19
+        Me.Button_Zero.Text = "0"
+        Me.Button_Zero.UseVisualStyleBackColor = True
+        '
+        'TextBox_Result
+        '
+        Me.TextBox_Result.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox_Result.Font = New System.Drawing.Font("MS UI Gothic", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox_Result.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox_Result.Location = New System.Drawing.Point(54, 47)
+        Me.TextBox_Result.Name = "TextBox_Result"
+        Me.TextBox_Result.ReadOnly = True
+        Me.TextBox_Result.Size = New System.Drawing.Size(239, 22)
+        Me.TextBox_Result.TabIndex = 20
+        Me.TextBox_Result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button_Clear
+        '
+        Me.Button_Clear.Location = New System.Drawing.Point(183, 185)
+        Me.Button_Clear.Name = "Button_Clear"
+        Me.Button_Clear.Size = New System.Drawing.Size(52, 47)
+        Me.Button_Clear.TabIndex = 21
+        Me.Button_Clear.Text = "Clear"
+        Me.Button_Clear.UseVisualStyleBackColor = True
+        '
+        'Button_Period
+        '
+        Me.Button_Period.Location = New System.Drawing.Point(67, 238)
+        Me.Button_Period.Name = "Button_Period"
+        Me.Button_Period.Size = New System.Drawing.Size(51, 48)
+        Me.Button_Period.TabIndex = 22
+        Me.Button_Period.Text = "."
+        Me.Button_Period.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(302, 294)
+        Me.Controls.Add(Me.Button_Period)
+        Me.Controls.Add(Me.Button_Clear)
+        Me.Controls.Add(Me.TextBox_Result)
+        Me.Controls.Add(Me.Button_Zero)
+        Me.Controls.Add(Me.Button_RightParen)
+        Me.Controls.Add(Me.Button_LeftParen)
+        Me.Controls.Add(Me.Button_Equal)
+        Me.Controls.Add(Me.Button_Slash)
+        Me.Controls.Add(Me.Button_Asterisk)
+        Me.Controls.Add(Me.Button_Minus)
+        Me.Controls.Add(Me.Button_Plus)
+        Me.Controls.Add(Me.Button_Nine)
+        Me.Controls.Add(Me.Button_Eight)
+        Me.Controls.Add(Me.Button_Seven)
+        Me.Controls.Add(Me.Button_Six)
+        Me.Controls.Add(Me.Button_Five)
+        Me.Controls.Add(Me.Button_Four)
+        Me.Controls.Add(Me.Button_Three)
+        Me.Controls.Add(Me.Button_Two)
+        Me.Controls.Add(Me.Button_One)
+        Me.Controls.Add(Me.TextBox_Work)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox_Work As TextBox
+    Friend WithEvents Button_One As Button
+    Friend WithEvents Button_Two As Button
+    Friend WithEvents Button_Three As Button
+    Friend WithEvents Button_Four As Button
+    Friend WithEvents Button_Five As Button
+    Friend WithEvents Button_Six As Button
+    Friend WithEvents Button_Seven As Button
+    Friend WithEvents Button_Eight As Button
+    Friend WithEvents Button_Nine As Button
+    Friend WithEvents Button_Plus As Button
+    Friend WithEvents Button_Minus As Button
+    Friend WithEvents Button_Asterisk As Button
+    Friend WithEvents Button_Slash As Button
+    Friend WithEvents Button_Equal As Button
+    Friend WithEvents Button_LeftParen As Button
+    Friend WithEvents Button_RightParen As Button
+    Friend WithEvents Button_Zero As Button
+    Friend WithEvents TextBox_Result As TextBox
+    Friend WithEvents Button_Clear As Button
+    Friend WithEvents Button_Period As Button
 End Class
--- AzCalc/Form1.vb
+++ AzCalc/Form1.vb
@@ -1,3 +1,157 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "A'z Calc"
+        TextBox_Work.Text = ""
+        TextBox_Result.Text = ""
 
+    End Sub
+
+    Private Sub Button_One_Click(sender As Object, e As EventArgs) Handles Button_One.Click
+        TextBox_Work.Text = TextBox_Work.Text + "1"
+
+    End Sub
+
+    Private Sub Button_Two_Click(sender As Object, e As EventArgs) Handles Button_Two.Click
+        TextBox_Work.Text = TextBox_Work.Text + "2"
+
+    End Sub
+
+    Private Sub Button_Three_Click(sender As Object, e As EventArgs) Handles Button_Three.Click
+        TextBox_Work.Text = TextBox_Work.Text + "3"
+
+    End Sub
+
+    Private Sub Button_Four_Click(sender As Object, e As EventArgs) Handles Button_Four.Click
+        TextBox_Work.Text = TextBox_Work.Text + "4"
+
+    End Sub
+
+    Private Sub Button_Five_Click(sender As Object, e As EventArgs) Handles Button_Five.Click
+        TextBox_Work.Text = TextBox_Work.Text + "5"
+
+    End Sub
+
+    Private Sub Button_Six_Click(sender As Object, e As EventArgs) Handles Button_Six.Click
+        TextBox_Work.Text = TextBox_Work.Text + "6"
+
+    End Sub
+
+    Private Sub Button_Seven_Click(sender As Object, e As EventArgs) Handles Button_Seven.Click
+        TextBox_Work.Text = TextBox_Work.Text + "7"
+
+    End Sub
+
+    Private Sub Button_Eight_Click(sender As Object, e As EventArgs) Handles Button_Eight.Click
+        TextBox_Work.Text = TextBox_Work.Text + "8"
+
+    End Sub
+
+    Private Sub Button_Nine_Click(sender As Object, e As EventArgs) Handles Button_Nine.Click
+        TextBox_Work.Text = TextBox_Work.Text + "9"
+
+    End Sub
+
+    Private Sub Button_Plus_Click(sender As Object, e As EventArgs) Handles Button_Plus.Click
+        TextBox_Work.Text = TextBox_Work.Text + "+"
+
+    End Sub
+
+    Private Sub Button_Slash_Click(sender As Object, e As EventArgs) Handles Button_Slash.Click
+        If (TextBox_Work.Text <> "") Then
+            TextBox_Work.Text = TextBox_Work.Text + "/"
+        End If
+
+    End Sub
+
+    Private Sub Button_Minus_Click(sender As Object, e As EventArgs) Handles Button_Minus.Click
+        TextBox_Work.Text = TextBox_Work.Text + "-"
+
+    End Sub
+
+    Private Sub Button_Asterisk_Click(sender As Object, e As EventArgs) Handles Button_Asterisk.Click
+        If (TextBox_Work.Text <> "") Then
+            TextBox_Work.Text = TextBox_Work.Text + "*"
+        End If
+    End Sub
+
+    Private Sub Button_LeftParen_Click(sender As Object, e As EventArgs) Handles Button_LeftParen.Click
+        TextBox_Work.Text = TextBox_Work.Text + "("
+
+    End Sub
+
+    Private Sub Button_RightParen_Click(sender As Object, e As EventArgs) Handles Button_RightParen.Click
+        TextBox_Work.Text = TextBox_Work.Text + ")"
+
+    End Sub
+
+    Private Sub Button_Equal_Click(sender As Object, e As EventArgs) Handles Button_Equal.Click
+        calc()
+
+    End Sub
+
+    Private Sub Button_Zero_Click(sender As Object, e As EventArgs) Handles Button_Zero.Click
+        TextBox_Work.Text = TextBox_Work.Text + "0"
+
+    End Sub
+
+    Private Sub Button_Clear_Click(sender As Object, e As EventArgs) Handles Button_Clear.Click
+        TextBox_Work.Text = ""
+
+    End Sub
+
+    Private Sub TextBox_Work_TextChanged(sender As Object, e As EventArgs) Handles TextBox_Work.TextChanged
+        Dim laststring As String = RTrim(TextBox_Work.Text)
+
+
+    End Sub
+
+    Private Sub calc()
+        '計算式
+        Dim exp As String = TextBox_Work.Text
+
+        Dim t As Type =
+            Type.GetTypeFromProgID("MSScriptControl.ScriptControl")
+        Dim obj As Object = Activator.CreateInstance(t)
+        t.InvokeMember("Language",
+            System.Reflection.BindingFlags.SetProperty,
+            Nothing,
+            obj,
+            New Object() {"vbscript"})
+        'Eval関数で計算を実行して結果を取得
+        Dim result As Double = CDbl(
+            t.InvokeMember("Eval",
+                System.Reflection.BindingFlags.InvokeMethod,
+                Nothing,
+                obj,
+                New Object() {exp}))
+
+        Dim str As String() = result.ToString.Split("."c)
+        Dim CustomFormat As String
+
+        ' 配列要素の確認
+        If (str.Length = 1) Then
+            ' 小数点以下が無い場合
+            CustomFormat = "#,0"
+        End If
+
+        If (str.Length > 1) Then
+            If str(1).Length > 10 Then
+                ' 小数点以下が11桁以上の場合
+                CustomFormat = "n10"
+            Else
+                ' 小数点以下が10桁以下の場合
+                CustomFormat = "n" + str(1).Length.ToString
+            End If
+        End If
+
+
+        '結果を表示
+        TextBox_Result.Text = result.ToString(CustomFormat)
+
+    End Sub
+
+    Private Sub Button_Period_Click(sender As Object, e As EventArgs) Handles Button_Period.Click
+        TextBox_Work.Text = TextBox_Work.Text + "."
+
+    End Sub
 End Class
